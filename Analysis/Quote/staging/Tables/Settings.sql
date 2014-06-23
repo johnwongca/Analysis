6a07af0ac103deda177c74f12c73ab2a03f9fb55
John Huang <John.Huang@sqlnotes.info>
2014-06-22 22:10:45 -0700

    Remove deprecated project

--- Analysis/Quote/staging/Tables/Settings.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [staging].[Settings] (
-    [Section]          VARCHAR (128) NOT NULL,
-    [Name]             VARCHAR (128) NOT NULL,
-    [Value]            VARCHAR (128) NOT NULL,
-    [LastModifiedDate] DATETIME      CONSTRAINT [DF_Settings_LastModifiedDate] DEFAULT (getdate()) NOT NULL,
-    CONSTRAINT [PK_Settings] PRIMARY KEY CLUSTERED ([Section] ASC, [Name] ASC)
-);
-
--- Analysis/QuoteStagingCLR/Functions/clr.GetLoopbackConnectionString.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE FUNCTION clr.GetLoopbackConnectionString()
-RETURNS varchar(max)
-AS
-BEGIN
-	return 'Server='+@@ServerName+';Database='+db_name()+';trusted_connection=true'
-END

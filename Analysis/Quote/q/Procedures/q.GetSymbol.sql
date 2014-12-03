6c8a2e58f9cb6c888ac460c3cba4511796675e72
John Huang <John.Huang@sqlnotes.info>
2014-12-02 19:26:05 -0800

    add more fields to getsymbol

--- Analysis/Quote/q/Procedures/q.GetSymbol.sql
+++ Analysis/Quote/q/Procedures/q.GetSymbol.sql
@@ -1,9 +1,11 @@
 ﻿create procedure q.GetSymbol
 as
 begin
-	select SymbolID, Exchange, Symbol, SymbolName, LongName, DayPriceLastUpdate, MinutePriceLastUpdate, DayPriceFirstUpdate, MinutePriceFirstUpdate
+	select s.SymbolID, s.Exchange, s.Symbol, s.SymbolName, s.LongName, s.DayPriceLastUpdate, s.MinutePriceLastUpdate, s.DayPriceFirstUpdate, s.MinutePriceFirstUpdate,
+		f.Dividend, f.DividendDate, f.DividendYield, f.DPS, f.EBITDA, f.MarketCap, f.EPS, f.PtS, f.NTA, f.PE, f.PEG, f.PtB, f.Shares, f.Yield
 	from q.Symbol s
-	where MinutePriceLastUpdate > dateadd(day, -7, getdate())
-		or DayPriceLastUpdate  > dateadd(day, -7, getdate())
-	order by Exchange, Symbol
+		left join q.Fundamental f on s.SymbolID = f.SymbolID and f.ExpiryDate is null
+	where s.MinutePriceLastUpdate > dateadd(day, -7, getdate())
+		or s.DayPriceLastUpdate  > dateadd(day, -7, getdate())
+	order by s.Exchange, s.Symbol
 end
